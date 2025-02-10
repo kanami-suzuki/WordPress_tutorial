b2f962a2ee3c5da6efffe23715ea3513b59b5873
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-02-10 22:25:36 +0900

    single.php

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+      <section>
+
+        <h2>これはsingle.phpです。</h2>
+        <p>このサイトの説明ページです。</p>
+
+
+      </section>
+
+    </div>
+    <!--/contents-->
+
+<?php get_footer(); ?>
